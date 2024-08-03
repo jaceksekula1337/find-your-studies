ea2dac9d5497c477283f4e1777683bd7fed96c67
Jacek <jacek.sekula@codewave.pl>
2024-08-03 03:04:55 +0200

    updated view and logic for filters

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,29 @@ import useCourses from "../hooks/useCourses";
 
 export default function Home() {
   const { courses, filteredCourses, loading, filterCourses } = useCourses();
-
+  
   return (
-    <div className="p-10 min-h-screen bg-gray-400 flex flex-col items-center">
-      <h1 className="text-4xl font-bold mb-8 text-blue-600">
-        Wyszukiwarka Kierunków
+    <div className="p-10 min-h-screen bg-violet-50 flex flex-col items-center  w-full">
+      <h1 className="text-5xl font-bold mb-8 text-center">
+        Wyszukaj kierunek studiów, <br></br>zobacz czy do niego pasujesz albo{" "}
+        <br></br>
+        sprawdź co zaproponuje ci LOS.
       </h1>
-      <SearchBar onSearch={filterCourses} allCourses={courses} />
+      <div className="w-full max-w-4xl">
+        <SearchBar onSearch={filterCourses} allCourses={courses} />
+      </div>
       {loading ? <p>Ładowanie...</p> : <CourseList courses={filteredCourses} />}
+      {/* Paginate below */}
+      <div className="flex justify-center mt-8">
+        <button className="mx-1">← Previous</button>
+        {/* Pagination numbers */}
+        {[...Array(5).keys()].map((page) => (
+          <button key={page} className="mx-1">
+            {page + 1}
+          </button>
+        ))}
+        <button className="mx-1">Next →</button>
+      </div>
     </div>
   );
 }
