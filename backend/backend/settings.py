1279cda2dc756350e8ff729a4614a4dea673a84c
Jacek <jacek.sekula@codewave.pl>
2025-06-14 18:19:12 +0200

    aloowed host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=6puh#3&1if3oml^i9gyc2=m18jyne@8*4c!@qxm&ehu0yb#%=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['find-your-studies-backend-production.up.railway.app']
 
 
 # Application definition
